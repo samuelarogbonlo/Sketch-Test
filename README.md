8513097b4664b922f3dd80b3bb4a08f23f20cae2
samuelarogbonlo <sbayo971@gmail.com>
2021-12-10 17:08:08 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Python script to move files between S3 buckets
+
+This script moves legacy assets to a production S3 bucket
+
+## How to run
+
+- Clone the github repository by running `git clone <repo_url>` in your terminal
+- Navigate to the folder containing the `move.py` file
+- Install dependencies with `pip3 install -r requirements.txt`
+- To seed dummy data into your database, make sure to set the environment variables defined in the `.env.example` file. Then, run the command `python3 seed/seeder.py <number_of_images>`. Set `<number_of_images>` to whatever number of images you want the seed script to create in your database.
+- Run the command `python3 move.py` in your terminal to run the `move.py` script
+
+## Development setup
+
+Resources used in this script were created manually including S3 buckets and PostgreSQL databases. However, in a production environment, it is strongly advised to follow infrastructure as code best practices.
--- move.py
+++ move.py
@@ -7,16 +7,28 @@ import psycopg2
 import os
 import sys
 import threading
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Env variables for database and S3 configuration
+# -----------------------------------
+# S3 buckets need to allow the IAM user access 
+# to read the source bucket and write to the destination bucket
+# -----------------------------------
 DB_CONN_STRING = os.getenv(
-    'DB_CONN_STRING', 'postgres://postgres:gopher@127.0.0.1/proddatabase')
-S3_SOURCE_BUCKET_NAME = os.getenv('S3_SOURCE_BUCKET_NAME', 'legacy-s3-ut')
-S3_DEST_BUCKET_NAME = os.getenv('S3_DEST_BUCKET_NAME', 'production-s3-ut')
+    'DB_CONN_STRING', 'postgres://username:userpassword@127.0.0.1/proddatabase')
+S3_SOURCE_BUCKET_NAME = os.getenv('S3_SOURCE_BUCKET_NAME', 'legacy-s3')
+S3_DEST_BUCKET_NAME = os.getenv('S3_DEST_BUCKET_NAME', 'production-s3')
 S3_OBJECT_DEST_PREFIX = os.getenv('S3_OBJECT_DEST_PREFIX', 'avatar')
 
 # Initializing S3 client
 s3 = boto3.client("s3")
+
+# -----------------------------------
+# NB: The database user needs to have read and write permissions in the database.
+# -----------------------------------
 
 # Create database connection
 try:
@@ -25,6 +37,10 @@ except Exception as e:
     logging.error(f"Error while connecting to the database: {e}")
     sys.exit(1)
 
+# -----------------------------------
+# This function checks for the existence of a path in the database
+# it returns a boolean to confirm the existence of the path.
+# -----------------------------------
 def check_exists(name):
     try:
         name_exists = False
@@ -35,51 +51,62 @@ def check_exists(name):
         conn.commit()
         return name_exists
     except Exception as e:
-        logging.error(f"Error inserting to the database: {e}")
+        logging.error(f"Error: {e}")
         sys.exit(1)
 
-def insert_db_row(new_path, old_path):
+# -----------------------------------
+# This updates the legacy path to the new path which is the
+# production path.
+# -----------------------------------
+def update_db_row(new_path, old_path):
     try:
         cur = conn.cursor()
-        cur.execute("UPDATE avatars SET path = (%s) WHERE path = (%s)", (new_path, old_path))
+        cur.execute(
+            "UPDATE avatars SET path = (%s) WHERE path = (%s)", (new_path, old_path))
         conn.commit()
     except Exception as e:
         print(f"Error inserting to the database: {e}")
         sys.exit(1)
 
+# -----------------------------------
+# This function copies files between S3 buckets (legacy and production).
+# -----------------------------------
 def copy_file(key):
     file_name = key.split("/")[-1]
     dest_key = f"{S3_OBJECT_DEST_PREFIX}/{file_name}"
     copy_source = {
-    'Bucket': S3_SOURCE_BUCKET_NAME,
-    'Key': key
+        'Bucket': S3_SOURCE_BUCKET_NAME,
+        'Key': key
     }
     try:
         if not check_exists(dest_key):
             response = s3.copy_object(
-            Bucket=S3_DEST_BUCKET_NAME,
-            Key=dest_key,
-            CopySource=copy_source
+                Bucket=S3_DEST_BUCKET_NAME,
+                Key=dest_key,
+                CopySource=copy_source
             )
             print(f"Successfully copied file {dest_key}")
-            insert_db_row(dest_key, key)
+            update_db_row(dest_key, key)
         else:
             print(f"The asset {dest_key} already exists")
     except Exception as e:
         logging.error(f"Error while copying file: {e}")
         sys.exit(1)
 
+# -----------------------------------
+# This function gets all the files that exists in the legacy bucket
+# -----------------------------------
 def get_files():
     files = []
     try:
         files = [file["Key"] for file in s3.list_objects(
-        Bucket=S3_SOURCE_BUCKET_NAME
+            Bucket=S3_SOURCE_BUCKET_NAME
         )['Contents'] if file["Key"] != "image/"]
     except Exception as e:
-        logging.error(f"Error while copying file: {e}")
+        logging.error(f"Error while getting files: {e}")
         sys.exit(1)
-
     return files
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
